c5e9446f22b695e4cecb6056bdec6d086ce39b01
sekeita <nasubafils@gmail.com>
2020-04-19 20:59:36 +1000

    dev activity

--- resources/config/groovy/baseURL.groovy
+++ resources/config/groovy/baseURL.groovy
@@ -7,5 +7,5 @@
 import jenkins.model.JenkinsLocationConfiguration
 
 JenkinsLocationConfiguration location = Jenkins.instance.getExtensionList('jenkins.model.JenkinsLocationConfiguration')[0]
-location.url = 'http://192.168.99.100:30001/' // minikube:jenkins-node-port
+location.url = 'http://192.168.247.129:30001/' // minikube:jenkins-node-port
 location.save()
